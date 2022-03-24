0f88995d370b79f4446c8f3656816ef64a3b801f
Abdulhameed123 <yusufhameed125@gmail.com>
2022-03-24 18:54:31 +0100

    first commit

--- contracts/scripts/sample-script.js
+++ contracts/scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Staking = await hre.ethers.getContractFactory("Staking");
+  const staking = await Staking.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  await staking.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Staking deployed to:", staking.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
